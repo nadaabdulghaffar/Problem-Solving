1a9d781f9ff0c38d26c924ae12170d66f2304019
Nada <nadaabdulghaffar42@gmail.com>
2024-08-27 12:32:50 +0300

    Problem x

--- ICPC Assiut University Community/Sheet #2 (Loops)/X.cpp
+++ ICPC Assiut University Community/Sheet #2 (Loops)/X.cpp
@@ -1,24 +1,68 @@
 #include <iostream>
 #include <bitset>
-using namespace std;
+#include <cmath>
+
+// Solution 1
 
 int main() {
     int T;
-    cin >> T; // Read the number of test cases
+    std::cin >> T;
     
     while (T--) {
         unsigned int N;
-        cin >> N; // Read the input number
+       std::cin >> N;
         
         // Convert N to binary and count the number of 1s
-        int count = bitset<32>(N).count();
+        int count = std::bitset<32>(N).count();
         
         // Generate a number that has 'count' number of 1s in its binary representation
         unsigned int result = (1 << count) - 1;
         
         // Output the result
-        cout << result << endl;
+        std::cout << result << std::endl;
     }
     
     return 0;
 }
+
+// -------------------------------------------------------------------------------------
+// Solution 2
+    int CountOnes(int n)
+    {
+        int count = 0;
+        while (n > 0)
+        {
+            count +=  n%2;
+            n /= 2;
+        }
+        return count;
+        
+    }
+
+
+    int main()
+    {
+
+        int t;
+        std::cin >> t;
+
+        while(t--)
+        {
+            int n;
+            std::cin >> n;
+
+            int count = CountOnes(n);
+        
+
+                int result = (1 << count) - 1;
+
+            std::cout << result << std::endl;
+        }
+
+
+
+        return 0;
+    }
+
+
+
