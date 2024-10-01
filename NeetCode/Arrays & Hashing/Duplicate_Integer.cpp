f0bf5a99e32dcde235de5210cb7adca1e8f3c7af
Nada <nadaabdulghaffar42@gmail.com>
2024-10-01 16:05:13 +0300

    problem 1

---
+++ NeetCode/Arrays & Hashing/Duplicate_Integer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+/**
+ * Time Complexity O(n ^ 2)
+ */
+class Solution
+{
+public:
+    bool hasDuplicate(std::vector<int> &inputArray)
+    {
+        for (int i = 0; i < inputArray.size(); i++)
+        {
+            for (int j = i + 1; j < inputArray.size(); j++)
+            {
+                if (inputArray[i] == inputArray[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+};
