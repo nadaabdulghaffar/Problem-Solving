9608fb8ac18381455da68b0068d3116fc9165786
Nada <nadaabdulghaffar42@gmail.com>
2024-12-20 21:57:08 +0200

    Two sum _optimized

--- NeetCode/Arrays & Hashing/two_sum.py
+++ NeetCode/Arrays & Hashing/two_sum.py
@@ -11,3 +11,18 @@ class Solution:
             diff = target - num
             if diff in Hashmap and Hashmap[diff] != index:
                 return [index, Hashmap[diff]] 
+            else:
+                Hashmap[num] = index
+
+# optimized solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        Hashmap = {}
+
+        for index, num in enumerate(nums):
+            diff = target - num
+            if diff in Hashmap and Hashmap[diff] != index:
+                return [Hashmap[diff], index] 
+            else:
+                Hashmap[num] = index
