f80e87517f4dbae150080c93be8dd2c9266b8ced
Nada <125583302+nadaabdulghaffar@users.noreply.github.com>
2025-09-04 17:46:58 +0300

    [110] [Time Beats: 100.00%] [Memory Beats: 26.58%] - LeetPush

---
+++ Leetcode/Balanced-Binary-Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # make sure all the sub-trees is balanced
+        def dfs(root):
+            if not root:
+                return [True, 0] # balanced & hight = 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            Balanced = ((left[0] and right[0]) and abs(left[1] - right[1]) <=1)
+
+            return [Balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
